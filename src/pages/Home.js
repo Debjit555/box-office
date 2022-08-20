bfb18b3340bd95655dafc2691caad82e3dd3e433
Debjit555 <debjitdas2001@yahoo.in>
2022-08-20 12:09:35 +0530

    created two new more hooks: useLastQuery and useShow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
-import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
-import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
   const isShowsSearch = searchOption === 'shows';
-
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
       setResults(result);
@@ -35,6 +35,7 @@ const Home = () => {
     if (results && results.length === 0) {
       return <div>No results</div>;
     }
+
     if (results && results.length > 0) {
       return results[0].show ? (
         <ShowGrid data={results} />
@@ -42,6 +43,7 @@ const Home = () => {
         <ActorGrid data={results} />
       );
     }
+
     return null;
   };
 
@@ -54,6 +56,7 @@ const Home = () => {
         onKeyDown={onKeyDown}
         value={input}
       />
+
       <div>
         <label htmlFor="shows-search">
           Shows
@@ -77,6 +80,7 @@ const Home = () => {
           />
         </label>
       </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
